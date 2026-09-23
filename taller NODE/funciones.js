626a39c6ec9b2f96f688e6b390276ec655533299
rcarreno4-dot <rcarreno4@udi.edu.co>
2026-09-22 21:26:03 -0500

    Add files via upload

---
+++ taller NODE/funciones.js
@@ -0,0 +1,11 @@
+function nombreFuncion(nombre) {
+  // Código de la función
+  return "Hola, " + nombre + "!";
+}
+
+function sumar(num1, num2) {
+  console.log("Suma de " + num1 + " y " + num2 + " es: " + (num1 + num2));
+  
+}
+console.log(nombreFuncion("Carlos"));
+sumar (10,20);
